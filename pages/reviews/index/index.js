435efaac5264e41647a24eb4425b06c36c5ecc41
Jason Fu <jasonguangfu@gmail.com>
2019-04-03 16:25:01 +0800

    added location, map base layout, and reviews

---
+++ pages/reviews/index/index.js
@@ -0,0 +1,77 @@
+// pages/reviews/index/index.js
+const app = getApp();
+Page({
+  
+  /**
+   * Page initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+    console.log('options', options)
+    const page = this;
+    const url = app.globalData.url;
+    wx.request({
+      url: `${url}trainings/${options.training_id}`,
+      method: 'GET',
+      success(res) {
+        const training = res.data;
+        page.setData(training);
+      }
+    })
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
