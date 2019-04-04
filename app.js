4e88278d27d14e4b66d201778ad19fc854782b44
Agnes Selvita <a.selvita@gmail.com>
2019-04-05 01:07:50 +0800

    basic form styling

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const AV = require('./utils/av-weapp-min.js')
-const config = require('./key')
-// Initialization of the app
+// const AV = require('./utils/av-weapp-min.js')
+// const config = require('./key')
+// // Initialization of the app
 
-AV.init({
-  appId: config.appId,
-  appKey: config.appSecret,
-});
+// AV.init({
+//   appId: config.appId,
+//   appKey: config.appSecret,
+// });
 
 App ({
   
   onLaunch: function () {
     // const host = 'http://localhost:3001/api/v1/'
 
-    // const host = 'http://localhost:3000/api/v1/'
-    const host = 'https://lentraineur.herokuapp.com/api/v1/'
+    const host = 'http://localhost:3000/api/v1/'
+    // const host = 'https://lentraineur.herokuapp.com/api/v1/'
 
     wx.login({
       success: (res) => {
@@ -34,8 +34,8 @@ App ({
   },
   globalData: {
     // url: 'http://localhost:3001/api/v1/'
-    // url: 'http://localhost:3001/api/v1/'
-    url: 'https://lentraineur.herokuapp.com/api/v1/'
+    url: 'http://localhost:3000/api/v1/'
+    // url: 'https://lentraineur.herokuapp.com/api/v1/'
   }
 })
 
